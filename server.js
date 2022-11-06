aa5fccf3ba9eab4aba263d462a5a5555161e3884
vishnu ck <vishnuck464@gmail.com>
2022-11-06 17:59:32 +0530

    cors url added

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const server =app.listen(PORT,()=>{
 })
 const io =new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://res-avatar-chatapp-static.onrender.com",
       credentials: true,
     },
   });
